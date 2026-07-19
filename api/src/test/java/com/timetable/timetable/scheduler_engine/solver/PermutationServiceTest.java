b4d8f468b25f9ec449bf13f1b77dd7fc853eeee9
Skylark <152505167+SideralSkylark@users.noreply.github.com>
2026-07-19 17:41:55 +0200

    fix: broken test case for applyCohortSwap

--- api/src/test/java/com/timetable/timetable/scheduler_engine/solver/PermutationServiceTest.java
+++ api/src/test/java/com/timetable/timetable/scheduler_engine/solver/PermutationServiceTest.java
@@ -1,16 +1,17 @@
-package com.timetable.timetable.sheduler_engine.solver;
+package com.timetable.timetable.scheduler_engine.solver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
+import com.timetable.timetable.domain.schedule.entity.CohortSubject;
 import com.timetable.timetable.domain.schedule.entity.ScheduledClass;
+import com.timetable.timetable.domain.schedule.entity.Subject;
 import com.timetable.timetable.domain.schedule.entity.Timeslot;
 import com.timetable.timetable.domain.schedule.repository.ScheduledClassRepository;
-import com.timetable.timetable.scheduler_engine.solver.PermutationService;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -29,13 +30,20 @@ class PremutationServiceTest {
   void shouldSwapTimeslotsBetweenScheduledClasses() {
     Timeslot timeslot1 = new Timeslot();
     timeslot1.setId(1L);
+    Timeslot timeslot2 = new Timeslot();
+    timeslot2.setId(2L);
+
+    CohortSubject cohortSubject1 = new CohortSubject();
+    cohortSubject1.setSubject(new Subject());
     ScheduledClass class1 = new ScheduledClass();
+    class1.setCohortSubject(cohortSubject1);
     class1.setId(1L);
     class1.setTimeslot(timeslot1);
 
-    Timeslot timeslot2 = new Timeslot();
-    timeslot2.setId(2L);
+    CohortSubject cohortSubject2 = new CohortSubject();
+    cohortSubject2.setSubject(new Subject());
     ScheduledClass class2 = new ScheduledClass();
+    class2.setCohortSubject(cohortSubject2);
     class2.setId(2L);
     class2.setTimeslot(timeslot2);
 
